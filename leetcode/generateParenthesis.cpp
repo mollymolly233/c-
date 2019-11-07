a19b8e8b7b755b6837e44e6b7b6733913635d821
jwh <mollymollyjwh@163.com>
2019-11-07 22:18:02 +0800

    update: generateParenthesis, adjust sequence

--- leetcode/generateParenthesis.cpp
+++ leetcode/generateParenthesis.cpp
@@ -5,22 +5,36 @@
 using namespace std;
 class Solution {
 public:
+	//暴力破解
 	vector<string> group;
     vector<string> generateParenthesis(int n);
     void generate(string origin,int len);
     bool validBrackets(string str);
+
+	//回溯法
+    vector<string> generateParenthesis2(int n);
+    void backtrack(vector<string> &ans, string cur, int open, int close, int max);
 };
 void showStrVec(vector<string> arr);
+
+
+
 
 int main(){
 	Solution solution;
 	int n = 3;
-	// solution.generate("",n*2);
-	showStrVec(solution.group);
-	solution.generateParenthesis(n);
+
+	vector<string> result = solution.generateParenthesis(n);
+	showStrVec(result);
+	cout << "=============================" <<endl;
+
+	vector<string> result2 = solution.generateParenthesis2(n);
+	showStrVec(result2);
 	return 0;
 }
 
+
+//==================暴力破解======================
 vector<string> Solution::generateParenthesis(int n) {
 	generate("",n*2);
 	return group;
@@ -44,7 +58,6 @@ bool Solution::validBrackets(string str){
 	if(leftCount == 0) return true;
 	return false;
 }
-
 //递归生成括号：完整的括号串为，第一个元素+后面n-1个括号的串
 void Solution::generate(string origin,int len){
 	if(len == 0) { group.push_back("");return;}
@@ -60,28 +73,30 @@ void Solution::generate(string origin,int len){
 	generate(origin+")",len-1);
 }
 
-void showStrVec(vector<string> arr){
-	cout << "vector<string>:" << endl;
-	for(int i = 0; i< (int)arr.size(); i++){
-		cout << arr[i] << endl;
-	}
-	cout << endl;
-}
-
-vector<string> Solution::generateParenthesis(int n) {
+//==================回溯法======================
+vector<string> Solution::generateParenthesis2(int n) {
     vector<string> ans;//新建结果数组
     backtrack(ans, "", 0, 0, n);
     return ans;
 }
-
-void Solution::backtrack(vector<string> ans, string cur, int open, int close, int max){
-    if (cur.length() == max * 2) {//递归出口，如果字符串长度==括号个数 = 括号对数*2
+void Solution::backtrack(vector<string> &ans, string cur, int open, int close, int max){
+    if ((int)cur.length() == max * 2) {//递归出口，如果字符串长度==括号个数 = 括号对数*2
         ans.push_back(cur);//符合递归出口时，把字符串正确的括号序列的字符串 添加进数组
         return;
     }
     //回溯法，
     if (open < max)//当左括号个数<总数对数时，贴一个左括号
         backtrack(ans, cur+"(", open+1, close, max);
-    if (close < open)//当右括号个数<右括号个数时，贴一个右括号
+    if (close < open)//当右括号个数<左括号个数时，贴一个右括号
         backtrack(ans, cur+")", open, close+1, max);
+}
+
+
+//调试代码
+void showStrVec(vector<string> arr){
+	cout << "vector<string>:" << endl;
+	for(int i = 0; i< (int)arr.size(); i++){
+		cout << arr[i] << endl;
+	}
+	cout << endl;
 }
