f4974ca3fe9fa7e5f6d7119462383c94cf6ad6b3
jwh <mollymollyjwh@163.com>
2019-11-07 22:05:41 +0800

    update: generateParenthesis with backtracking algrithm

--- leetcode/generateParenthesis.cpp
+++ leetcode/generateParenthesis.cpp
@@ -67,3 +67,21 @@ void showStrVec(vector<string> arr){
 	}
 	cout << endl;
 }
+
+vector<string> Solution::generateParenthesis(int n) {
+    vector<string> ans;//新建结果数组
+    backtrack(ans, "", 0, 0, n);
+    return ans;
+}
+
+void Solution::backtrack(vector<string> ans, string cur, int open, int close, int max){
+    if (cur.length() == max * 2) {//递归出口，如果字符串长度==括号个数 = 括号对数*2
+        ans.push_back(cur);//符合递归出口时，把字符串正确的括号序列的字符串 添加进数组
+        return;
+    }
+    //回溯法，
+    if (open < max)//当左括号个数<总数对数时，贴一个左括号
+        backtrack(ans, cur+"(", open+1, close, max);
+    if (close < open)//当右括号个数<右括号个数时，贴一个右括号
+        backtrack(ans, cur+")", open, close+1, max);
+}
